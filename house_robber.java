6bf3be8226d498eafb14b00027e33450dd4c91bc
Tejas Mesta <tejasmesta@gmail.com>
2022-07-27 17:25:02 +0530

    Add files via upload

---
+++ house_robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        int ans = 0;
+        
+        int n = nums.length;
+        
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for(int i=1;i<n;i++)
+        {
+            dp[i+1] = Math.max(dp[i],dp[i-1] + nums[i]);
+        }       
+              
+        return dp[n];
+        
+    }
+}
